cf685b5af23e6e0e74af10360d6325b96f3e49f0
Özgür Deniz Günseli <dgunseli@gmail.com>
2020-11-10 09:31:30 +0000

    Delete pickup messages was implemented

--- aries_cloudagent/protocols/pickup/message_types.py
+++ aries_cloudagent/protocols/pickup/message_types.py
@@ -8,6 +8,8 @@ BATCH_PICKUP_REQUEST = f"{PROTOCOL_URI}/batch_pickup_request"
 BATCH_PICKUP_RESPONSE = f"{PROTOCOL_URI}/batch_pickup_response"
 LIST_PICKUP_REQUEST = f"{PROTOCOL_URI}/list_pickup_request"
 LIST_PICKUP_RESPONSE = f"{PROTOCOL_URI}/list_pickup_response"
+DELETE_PICKUP_REQUEST = f"{PROTOCOL_URI}/delete-request"
+DELETE_PICKUP_RESPONSE = f"{PROTOCOL_URI}/delete-response"
 
 NEW_PROTOCOL_URI = "https://didcomm.org/messagepickup/0.1"
 
@@ -15,8 +17,10 @@ NEW_STATUS_REQUEST = f"{NEW_PROTOCOL_URI}/status_request"
 NEW_STATUS_RESPONSE = f"{PROTOCOL_URI}/status_response"
 NEW_BATCH_PICKUP_REQUEST = f"{NEW_PROTOCOL_URI}/batch_pickup_request"
 NEW_BATCH_PICKUP_RESPONSE = f"{NEW_PROTOCOL_URI}/batch_pickup_response"
-NEW_LIST_PICKUP_REQUEST = f"{PROTOCOL_URI}/list_pickup_request"
-NEW_LIST_PICKUP_RESPONSE = f"{PROTOCOL_URI}/list_pickup_response"
+NEW_LIST_PICKUP_REQUEST = f"{NEW_PROTOCOL_URI}/list_pickup_request"
+NEW_LIST_PICKUP_RESPONSE = f"{NEW_PROTOCOL_URI}/list_pickup_response"
+NEW_DELETE_PICKUP_REQUEST = f"{NEW_PROTOCOL_URI}/delete-request"
+NEW_DELETE_PICKUP_RESPONSE = f"{NEW_PROTOCOL_URI}/delete-response"
 
 PROTOCOL_PACKAGE = "aries_cloudagent.protocols.pickup"
 
@@ -39,6 +43,12 @@ MESSAGE_TYPES = {
     LIST_PICKUP_RESPONSE: (
         f"{PROTOCOL_PACKAGE}.messages.list_pickup_response.ListPickupResponse"
     ),
+    DELETE_PICKUP_REQUEST: (
+        f"{PROTOCOL_PACKAGE}.messages.delete_pickup_request.DeletePickupRequest"
+    ),
+    DELETE_PICKUP_RESPONSE: (
+        f"{PROTOCOL_PACKAGE}.messages.delete_pickup_response.DeletePickupResponse"
+    ),
     NEW_STATUS_REQUEST: (
         f"{PROTOCOL_PACKAGE}.messages.status_request.StatusRequest"
     ),
@@ -56,5 +66,11 @@ MESSAGE_TYPES = {
     ),
     NEW_LIST_PICKUP_RESPONSE: (
         f"{PROTOCOL_PACKAGE}.messages.list_pickup_response.ListPickupResponse"
+    ),
+    NEW_DELETE_PICKUP_REQUEST: (
+        f"{PROTOCOL_PACKAGE}.messages.delete_pickup_request.DeletePickupRequest"
+    ),
+    NEW_DELETE_PICKUP_RESPONSE: (
+        f"{PROTOCOL_PACKAGE}.messages.delete_pickup_response.DeletePickupResponse"
     ),
 }
